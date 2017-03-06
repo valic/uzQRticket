4cf32042a91245f7308299cb4a8096f4325c8dc1
valic <valic.my@gmail.com>
2017-03-06 23:44:17 +0200

    Добавил Core Data

--- uzQRticket/QRscanViewController.swift
+++ uzQRticket/QRscanViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import CoreData
 
 class QRscanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
@@ -143,33 +144,163 @@ class QRscanViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
                 textQR = metadataObj.stringValue!
                 
                 if let textQR = textQR {
-                    print(textQR)
+                    //   print(textQR)
                     
+                    let arrayScanCode = textQR.components(separatedBy: "\n")
                     
-                    
+                    if arrayScanCode.count == 20 /*&& arrayScanCode[15] == arrayScanCode[19] */{
+                        
+                        // create an instance of our managedObjectContext
+                        let context = getContext ()
+                        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Tickets")
+                        
+                        // we set up our entity by selecting the entity and context that we're targeting
+                        let entity = NSEntityDescription.insertNewObject(forEntityName: "Tickets", into: context) as! Tickets
+                        
+                        let predicate = NSPredicate(format: "ticketID == %@", arrayScanCode[15])
+                        request.predicate = predicate
+                        do {
+                            let results = try context.fetch(request) as! [Tickets]
+                            
+                            
+                            if (results.count > 0) {
+                                
+                                self.deleteCaptureSession ()
+                                alertCaptureSession("Билет уже добавлен")
+                                
+                                
+                            } else {
+                                print("билета нету в Core Data, добавляем")
+                                
+                                deleteCaptureSession ()
+                                videoPreviewLayer!.removeFromSuperlayer()
+                                
+                                entity.setValue(textQR, forKey: "stringTicket") // сохраняем всю строку, для создания QR кода
+                                entity.setValue(arrayScanCode[1], forKey: "train") // поїзд
+                                entity.setValue(arrayScanCode[2], forKey: "departure") // відправлення
+                                entity.setValue(arrayScanCode[3], forKey: "destination") // призначення
+                                entity.setValue(stringToDate(arrayScanCode[4]), forKey: "dateTimeDep") // датаЧасВідпр
+                                entity.setValue(stringToDate(arrayScanCode[5]), forKey: "dateTimeDes") // датаЧасПриб
+                                entity.setValue(arrayScanCode[6], forKey: "coach") // вагон
+                                entity.setValue(arrayScanCode[7], forKey: "seat") // місце
+                                entity.setValue(arrayScanCode[9], forKey: "surnameAndName") // прізвищеІмя
+                                entity.setValue(NSString(string: arrayScanCode[12]).floatValue, forKey: "cost") // вартість
+                                entity.setValue(arrayScanCode[15], forKey: "ticketID")
+                                
+                                
+                                
+                                // we save our entity
+                                do {
+                                    try context.save()
+                                } catch {
+                                    fatalError("Failure to save context: \(error)")
+                                }
+                                
+                                // перейти на первый tab
+                                tabBarController?.selectedIndex = 0
+                                
+                            }
+                        } catch let error as NSError {
+                            // failure
+                            print("Fetch failed: \(error.localizedDescription)")
+                        }
+                        
+                        
+                    }
+                    else{
+                        if arrayScanCode.count == 1 && arrayScanCode[0].characters.count == 19 {
+                            
+                            self.alertCaptureSession("возможно шрих код")
+                            print(arrayScanCode[0]) //19
+                        }
+                        else {
+                            self.alertCaptureSession("в QR code нету билета")
+                        }
+                        
+                    }
                 }
-            }
+                //  captureSession!.stopRunning()
+                //  videoPreviewLayer!.removeFromSuperlayer()
                 
-            else{
-                print("del")
-                deleteCaptureSession ()
-                videoPreviewLayer!.removeFromSuperlayer()
+                // вернутся назад
+                let transition = CATransition()
+                transition.duration = 0.25
+                transition.type = kCATransitionPush
+                transition.subtype = kCATransitionFromLeft
+                view.window!.layer.add(transition, forKey: kCATransition)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let controller = storyboard.instantiateViewController(withIdentifier: "Ticket")
+                self.present(controller, animated: false, completion: nil)
+
+                
             }
         }
+            
+        else{
+            print("del")
+            deleteCaptureSession ()
+            videoPreviewLayer!.removeFromSuperlayer()
+        }
+    }
+    
+    func stringToDate (_ stringData: String) -> Date  {
         
+        let dateFormatter = DateFormatter()
         
+        dateFormatter.dateFormat  = "dd.MM HH:mm" //21.11 20:01
+        dateFormatter.timeZone = TimeZone(identifier: "Europe/Kiev")
+        dateFormatter.defaultDate = Date()
+        let date = dateFormatter.date(from: stringData)
         
+        var dayComponenet = DateComponents()
+        dayComponenet.day = 30
         
-        /*
-         // MARK: - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destinationViewController.
-         // Pass the selected object to the new view controller.
-         }
-         */
+        let theCalendar = Calendar.current
+        let nextDate = (theCalendar as NSCalendar).date(byAdding: dayComponenet, to: Date(), options: [])
+        // print(nextDate)
         
-}
+        if date!.compare(nextDate!) == .orderedDescending {
+            // Текущая дата больше конечной даты
+            
+            var dayComponenet = DateComponents()
+            dayComponenet.year = -1
+            
+            let theCalendar = Calendar.current
+            dateFormatter.defaultDate = (theCalendar as NSCalendar).date(byAdding: dayComponenet, to: Date(), options: [])
+            // print(dateFormatter.dateFromString(stringData)!)
+            return dateFormatter.date(from: stringData)!
+        }
+        else {
+            return date!
+        }
+    }
+    func alertCaptureSession(_ messageText: String)
+    {
+        let alertController = UIAlertController(title: title, message: messageText, preferredStyle:UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+        { action -> Void in
+            // Put your code here
+            self.initializationCaptureSession()
+        })
+        //self.captureSession!.startRunning()
+        self.present(alertController, animated: true, completion: nil)
+    }
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
 }
 
